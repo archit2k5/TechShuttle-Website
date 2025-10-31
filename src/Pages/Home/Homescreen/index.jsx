0c8f9f04c10286fada8b474201f65cf4248afa0f
archit2k5 <architsingh23993@gmail.com>
2025-10-31 11:38:41 +0000

    small fix

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <HeroSection />
       <Team />
       <AboutMe />
-      <Events />
+      {/* <Events /> */}
       <EventPics />
       <Footer />
     </>
